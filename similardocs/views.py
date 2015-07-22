f5ef64c6a3452c16a22831d3d4bc0cc3b3954bac
Trent Niemeyer <trent.niemeyer@gmail.com>
2015-07-22 13:25:35 -0600

    cleaned imports

--- similardocs/views.py
+++ similardocs/views.py
@@ -1,10 +1,6 @@
-from django.http import HttpResponse
-from simserver import SessionServer
-from gensim import utils
 import Pyro4
 
 from django.http import Http404, HttpResponse
-import datetime
 
 def index(request, url):
     server = Pyro4.Proxy(Pyro4.locateNS().lookup('gensim.testserver'))
